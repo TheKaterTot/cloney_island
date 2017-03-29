876f218f3f35597659b043011fd96a4cd02c0f89
blackknight75 <devbox4714@gmail.com>
2017-03-28 18:57:38 -0600

    guest can visit root and see login"

--- spec/home/guest_visits_root_spec.rb
+++ spec/home/guest_visits_root_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-describe 'when a guest visits the root page' do
-  # it 'they see login in navbar' do
-  #   visit '/'
-  #
-  #   within('.navbar') do
-  #     expect(page).to have_content('login')
-  #   end
-  # end
+feature 'when a guest visits the root page' do
+  scenario 'they see login in navbar' do
+    visit '/'
+
+    within('.nav-wrapper') do
+      expect(page).to have_content('Login')
+    end
+
+  end
 end

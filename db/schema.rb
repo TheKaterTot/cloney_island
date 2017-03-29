35d7601226ed9bd4f455594f5c956afff3a83ae7
Andy Wooten <andrewdwooten@icloud.com>
2017-03-28 19:51:17 -0600

    Modify comments associations
    Modified create_comments migration to implement
    appropriate polymorphic relationships for
    comments. Comments will always be associated
    with a user and only with either a question
    or an answer but never both. To access a
    comments parent, you have to call
    '.commentable' on the comment to return
    either its answer or question parent
    object as appropriate. This commit
    also enables bcrypt and implements
    the 'has_secure_password' method in the user
    model. This commit also adds a completed
    seed file. Seeding the database will
    create 1000 users with the 'registered_user'
    role, 5 questions asked per user, 5 questions
    answered per user, 1 comment per question, and
    1 comment per answer, and an admin as
    the role 'admin' as prescribed in the spec.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328224039) do
+ActiveRecord::Schema.define(version: 20170328224242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,11 @@ ActiveRecord::Schema.define(version: 20170328224039) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
-    t.integer  "answer_id"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["answer_id"], name: "index_comments_on_answer_id", using: :btree
-    t.index ["question_id"], name: "index_comments_on_question_id", using: :btree
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
@@ -48,6 +47,21 @@ ActiveRecord::Schema.define(version: 20170328224039) do
     t.integer "category_id"
     t.index ["category_id"], name: "index_questions_on_category_id", using: :btree
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "role_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id", using: :btree
+    t.index ["user_id"], name: "index_user_roles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,9 +77,9 @@ ActiveRecord::Schema.define(version: 20170328224039) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
-  add_foreign_key "comments", "answers"
-  add_foreign_key "comments", "questions"
   add_foreign_key "comments", "users"
   add_foreign_key "questions", "categories"
   add_foreign_key "questions", "users"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end

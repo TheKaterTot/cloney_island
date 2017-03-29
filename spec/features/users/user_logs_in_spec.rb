8b8d9fc97dce7bb1076da28326e1e490223ca6ed
Andy Wooten <andrewdwooten@icloud.com>
2017-03-29 17:03:21 -0600

    Complete user login
    Implements all necessary components
    for user login. We chose to bypass
    building in the validation of user
    roles until necessitated by role
    privelege implementation. Test
    is currently passing.

---
+++ spec/features/users/user_logs_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'user visits root' do
+  scenario 'user logs in' do
+    user = Fabricate(:user, password: 'password')
+
+    visit '/'
+
+    click_link 'Login'
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "Name", with: "#{user.name}"
+    fill_in "Password", with: "password"
+
+    click_button "Login"
+    expect(current_path).to eq(user_path(user))
+    byebug
+  end
+end

9ed335c4df56dad1fedd09e8cc07f77d233b17fc
Ryan Spink <ryanspinktech@gmail.com>
2017-03-30 10:24:01 -0600

    sad paths for asking question and posting answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :comments, as: :commentable
+  validates :body, presence: true
 end
--- spec/features/users/user_posts_question_spec.rb
+++ spec/features/users/user_posts_question_spec.rb
@@ -6,7 +6,7 @@ feature "user visits root and clicks 'Ask a question'" do
     user = Fabricate(:user, id: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    
+
     visit root_path
 
     within("#nav-mobile.left") do
@@ -33,6 +33,32 @@ feature "user visits root and clicks 'Ask a question'" do
     within("#question-body") do
       expect(page).to have_content("Why must I be a teenager in love?")
       expect(page).to_not have_content("Anything")
+    end
+  end
+
+  feature "and they incorrectly enter question parameters" do
+    scenario "they cannot create a question" do
+      Fabricate(:category, name: "Dogs")
+      user = Fabricate(:user, id: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      within("#nav-mobile.left") do
+        click_link("Ask Question")
+      end
+
+      expect(current_path).to eq(new_question_path)
+
+      fill_in("Body", :with => "Why must I be a teenager in love?")
+      select("Dogs", :from => "question_category_id")
+
+      click_button("Submit")
+
+      expect(current_path).to eq(new_question_path)
+
+      expect(page).to have_content("Failed to create question")
     end
   end
 end

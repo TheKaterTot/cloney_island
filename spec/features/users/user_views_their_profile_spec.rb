a55f3da4aeb99afd94e92a2d06353eff83b174f6
Andy Wooten <andrewdwooten@icloud.com>
2017-03-30 17:33:19 -0600

    Add user questions to user show
    Add card to hold and display a table
    of the users 5 most recent questions.
    The question title is displayed in a table
    and is linked to the question show page
    for that question. All tests are currently
    passing. This view is extracted to a partial
    _user-show-questions

--- spec/features/users/user_views_their_profile_spec.rb
+++ spec/features/users/user_views_their_profile_spec.rb
@@ -24,6 +24,31 @@ feature 'user profile' do
     visit user_path(user)
 
     expect(page).to have_css('#user-avatar')
+  end
 
+  scenario 'user sees their 5 most recent question titles as links' do
+    user = Fabricate(:user)
+    question1 = Fabricate(:question, user: user, title: 'bad')
+    question2 = Fabricate(:question, user: user, title: 'test2')
+    question3 = Fabricate(:question, user: user, title: 'test3')
+    question4 = Fabricate(:question, user: user, title: 'test4')
+    question5 = Fabricate(:question, user: user, title: 'test5')
+    question6 = Fabricate(:question, user: user, title: 'test6')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to have_css('#user-show-questions')
+
+    within('#user-show-questions') do
+      expect(page).to_not have_content(question1.title)
+
+      expect(page).to have_link(question6.title, href: question_path(question6))
+      expect(page).to have_link(question5.title, href: question_path(question5))
+      expect(page).to have_link(question4.title, href: question_path(question4))
+      expect(page).to have_link(question3.title, href: question_path(question3))
+      expect(page).to have_link(question2.title, href: question_path(question2))
+    end
   end
 end

a6d969619573eb773243755c1063a02a432ee161
Andy Wooten <andrewdwooten@icloud.com>
2017-03-31 06:59:12 -0600

    Add comment model tests
    Adds two tests to comment model spec.
    These test te two new methods added to
    the comment model for the purpose of
    retrieving and displaying comments
    made on a users activity.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,4 +9,51 @@ RSpec.describe Comment, type: :model do
   context "validations" do
     it { should validate_presence_of(:body) }
   end
+
+  describe '#source_question' do
+    it 'returns the source question of a comment' do
+      user = Fabricate(:user)
+      question = Fabricate(:question)
+      answer = Fabricate(:answer, question: question)
+      comment1 = question.comments.create(body:         'test',
+                                          user:         user )
+      comment2 = answer.comments.create(body:         'test',
+                                        user:         user )
+
+
+      expect(comment1.source_question).to eq(question)
+      expect(comment2.source_question).to eq(question)
+    end
+  end
+
+  describe '#comments_to_user_activity' do
+    it 'returns collection of comments made on user activity' do
+      user = Fabricate(:user)
+      user2 = Fabricate(:user)
+      question1 = Fabricate(:question, title: 'test', user: user)
+      answer1 = Fabricate(:answer, user: user, question: question1)
+      user2.comments.create(body: 'test', commentable: question1)
+      user2.comments.create(body: 'test', commentable: question1)
+      user2.comments.create(body: 'test', commentable: question1)
+      user2.comments.create(body: 'test', commentable: answer1)
+      user2.comments.create(body: 'test', commentable: answer1)
+
+      comments = Comment.comments_to_user_activity(user.id)
+
+
+      expect(comments.count).to eq(5)
+      
+      expect(comments[0].source_question).to eq(question1)
+      expect(comments[1].source_question).to eq(question1)
+      expect(comments[2].source_question).to eq(question1)
+      expect(comments[3].source_question).to eq(question1)
+      expect(comments[4].source_question).to eq(question1)
+
+      expect(comments[0].commentable).to eq(question1)
+      expect(comments[1].commentable).to eq(question1)
+      expect(comments[2].commentable).to eq(question1)
+      expect(comments[3].commentable).to eq(answer1)
+      expect(comments[4].commentable).to eq(answer1)
+    end
+  end
 end

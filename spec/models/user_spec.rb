611212faf78d69b36f9a1d923493497ab17f3991
Andy Wooten <andrewdwooten@icloud.com>
2017-03-31 07:05:54 -0600

    Add user model test
    Adds test for method added to user
    model to retrieve recent comments to
    a users activity. All tests are
    currently passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:phone) }
   end
+
+  describe '#comments_to_recent_activity' do
+    it 'returns 5 most recent comments to user activity' do
+      user = Fabricate(:user)
+      user2 = Fabricate(:user)
+      question1 = Fabricate(:question, title: 'test', user: user)
+      answer1 = Fabricate(:answer, user: user, question: question1)
+      user2.comments.create(body: 'test', commentable: question1)
+      user2.comments.create(body: 'test', commentable: question1)
+      user2.comments.create(body: 'test', commentable: answer1)
+      user2.comments.create(body: 'test', commentable: answer1)
+      user2.comments.create(body: 'test', commentable: answer1)
+
+      comments = user.comments_to_recent_activity
+
+      expect(comments.count).to eq(5)
+
+      expect(comments[0].source_question.user).to eq(user)
+      expect(comments[1].source_question.user).to eq(user)
+      expect(comments[2].source_question.user).to eq(user)
+      expect(comments[3].source_question.user).to eq(user)
+      expect(comments[4].source_question.user).to eq(user)
+    end
+  end
 end

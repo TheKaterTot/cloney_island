95b3ac8f9466b072dcbe2d96e68d17d740becaf5
Andy Wooten <andrewdwooten@icloud.com>
2017-03-31 07:29:59 -0600

    Modify user model test
    Change heading of comments box on user
    show page to be more explicit about content.
    Modify user model test specific to
    comments_to_recent_activity method to
    me more specific in indicating that the
    method gets the most recent comments
    based on updated_at time.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,11 +21,25 @@ RSpec.describe User, type: :model do
       user2 = Fabricate(:user)
       question1 = Fabricate(:question, title: 'test', user: user)
       answer1 = Fabricate(:answer, user: user, question: question1)
-      user2.comments.create(body: 'test', commentable: question1)
-      user2.comments.create(body: 'test', commentable: question1)
-      user2.comments.create(body: 'test', commentable: answer1)
-      user2.comments.create(body: 'test', commentable: answer1)
-      user2.comments.create(body: 'test', commentable: answer1)
+      user2.comments.create(body: 'test',
+                            commentable: question1,
+                            updated_at: '2017-03-21 19:55:40')
+      user2.comments.create(body: 'test',
+                            commentable: question1,
+                            updated_at: '2017-03-22 19:55:40')
+      user2.comments.create(body: 'test',
+                            commentable: question1,
+                            updated_at: '2017-03-23 19:55:40')
+      user2.comments.create(body: 'test',
+                            commentable: question1,
+                            updated_at: '2017-03-24 19:55:40')
+      user2.comments.create(body: 'test',
+                            commentable: question1,
+                            updated_at: '2017-03-25 19:55:40')
+
+      old_comment = user2.comments.create(body: 'test',
+                                          commentable: answer1,
+                                          updated_at: '2017-03-26 19:55:40')
 
       comments = user.comments_to_recent_activity
 
@@ -36,6 +50,8 @@ RSpec.describe User, type: :model do
       expect(comments[2].source_question.user).to eq(user)
       expect(comments[3].source_question.user).to eq(user)
       expect(comments[4].source_question.user).to eq(user)
+
+      expect(comments.include?(old_comment)).to eq(false)
     end
   end
 end

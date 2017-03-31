6857c89b8dc2308ad21fd33dde942253b3301742
Andy Wooten <andrewdwooten@icloud.com>
2017-03-30 18:10:42 -0600

    Add answer links to user show page
    Creates partial for display of users
    5 most recent answers. The answer
    does not have a title, so I chose
    to reference the title of the
    question with a link to the question.
    all tests are currently passing.

--- spec/features/users/user_views_their_profile_spec.rb
+++ spec/features/users/user_views_their_profile_spec.rb
@@ -51,4 +51,36 @@ feature 'user profile' do
       expect(page).to have_link(question2.title, href: question_path(question2))
     end
   end
+
+  scenario 'user sees their 5 most recent questions answered as links' do
+    user = Fabricate(:user)
+    question1 = Fabricate(:question, title: 'test')
+    question2 = Fabricate(:question)
+    question3 = Fabricate(:question)
+    question4 = Fabricate(:question)
+    question5 = Fabricate(:question)
+    question6 = Fabricate(:question)
+    answer1 = Fabricate(:answer, user: user, question: question1)
+    answer2 = Fabricate(:answer, user: user, question: question2)
+    answer3 = Fabricate(:answer, user: user, question: question3)
+    answer4 = Fabricate(:answer, user: user, question: question4)
+    answer5 = Fabricate(:answer, user: user, question: question5)
+    answer6 = Fabricate(:answer, user: user, question: question6)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to have_css('#user-show-answers')
+
+    within('#user-show-answers') do
+      expect(page).to_not have_content(question1.title)
+
+      expect(page).to have_link(question6.title, href: question_path(question6))
+      expect(page).to have_link(question5.title, href: question_path(question5))
+      expect(page).to have_link(question4.title, href: question_path(question4))
+      expect(page).to have_link(question3.title, href: question_path(question3))
+      expect(page).to have_link(question2.title, href: question_path(question2))
+    end
+  end
 end

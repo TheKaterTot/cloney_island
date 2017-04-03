d1cf48d131a216e269541224de3f5e9f8c9f4532
Ryan Spink <ryanspinktech@gmail.com>
2017-04-02 21:36:45 -0600

    adds feature testing for users
    upvoting on questions, comments on questions, answers, and comments on answers

--- spec/features/users/questions/user_can_vote_on_comments_to_question_spec.rb
+++ spec/features/users/questions/user_can_vote_on_comments_to_question_spec.rb
@@ -8,20 +8,21 @@ feature 'user views a question' do
     @user.roles.create(name: 'registered_user')
     category = Fabricate(:category)
     question = @user.questions.create(title:"eh", body:"what", category:category)
+    comment = question.comments.create(body:"hello", user_id:2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit question_path(question)
 
-    within("#question-upvote") do
+    within("#question-comment-upvote") do
       expect(page).to have_button("Upvote")
     end
 
-    within("#question-downvote") do
+    within("#question-comment-downvote") do
       expect(page).to have_button("Downvote")
     end
 
-    within("#question-upvote") do
+    within("#question-comment-upvote") do
       click_button("Upvote")
     end
 
@@ -29,16 +30,16 @@ feature 'user views a question' do
 
     expect(user.reputation_count(user)).to eq(1)
 
-    expect(page).to_not have_css("#question-upvote")
+    expect(page).to_not have_css("#question-comment-upvote")
 
-    within("#question-downvote") do
+    within("#question-comment-downvote") do
       expect(page).to have_button("Downvote")
       click_button("Downvote")
     end
 
     expect(current_path).to eq(question_path(question))
 
-    expect(page).to_not have_css("#question-downvote")
+    expect(page).to_not have_css("#question-comment-downvote")
 
     expect(user.reputation_count(user)).to eq(-1)
   end

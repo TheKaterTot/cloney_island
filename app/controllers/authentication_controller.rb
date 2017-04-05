39898535fe6e9170dc7006c0152f31d43d94bdda
Andy Wooten <andrewdwooten@icloud.com>
2017-04-05 14:32:19 -0600

    Modify flash messages
    Modifies flash message in authentication message to
    be called with flash.now so that it persists
    appropriately. Adds flash message to Users#create
    to remind new users to validate their account by clicking
    the link in their e-mail. All tests currently passing.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
   def update
     @user = User.find(params[:id])
     UserRole.create(user_id:@user.id, role_id:1)
-    flash[:success] = 'Your account has been authenticated!'
+    flash.now[:success] = 'Your account has been authenticated!'
     render 'users/show'
   end
 end
